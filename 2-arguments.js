454a4b6d4a24613a7755a7095b7b78525b3ac6d9
David Akande Sunday <akandedavsun00@gmail.com>
2025-05-22 21:16:47 +0100

    Added 2-arguments.js files

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+const Calculate = () =>{
+    console.log("No agument")
+}
+
+const Calc = (name) =>{
+    console.log("Argument found")
+}
+const Calcb = (name, place) =>{
+    console.log("Arguments found")
+}
