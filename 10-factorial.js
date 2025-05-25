15dd989a2e903207f97dff42d97b42ef92f20f44
David Akande Sunday <akandedavsun00@gmail.com>
2025-05-25 23:02:27 +0100

    added 10-factorials.js

---
+++ 10-factorial.js
@@ -0,0 +1,10 @@
+const args = process.argv[2];
+const num = Number(args);
+const factorial=num =>{
+    i=1
+    while (i<=num){
+        let total =0
+        total *= i
+    }
+    console.log(`The factorial is ${total}`)
+}
